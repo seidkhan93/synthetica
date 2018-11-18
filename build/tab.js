199d75d6ea108e1acb2fcb4839e54f2a6928cebe
Banana <dana140198@gmail.com>
2018-11-18 23:17:09 +0600

    add final version of main section

--- build/tab.js
+++ build/tab.js
@@ -3,36 +3,44 @@ const contPath = ".main__content";
 
 var dict = [
     {
+        id:   0,
+        title: "Curated goodies for Designers & Developers",
+        buttonText: "latest collection",
+        paragraph: "Synthetica is an experience design collective, bringing you the latest product, interaction and visual design goodies straight to your inbox."
+    },
+    {
         id:   1,
-        title: "Teehan + Lax",
-        buttonText: "“Sed risus feugiat fusce eu sit conubia venenatis aliquet nisl cras eu adipiscing ac cras at sem cras per”",
-        paragraph: "../src/icons/tabs/0.jpg"
+        title: "What is going on?",
+        buttonText: "wake up",
+        paragraph: "Where I am?"
     },
     {
         id:   2,
-        title: "Lex Luthor",
-        buttonText: "“hat should be upside down. We know better now, don't we? Devils don't come from hell beneath us. No, they come from the sky.”",
-        paragraph: "../src/icons/tabs/1.jpg"
+        title: "The Question",
+        buttonText: "lay like a stone",
+        paragraph: "Lay like a stone or Burn like a star"
     },
     {
         id:   3,
         title: "Tachikoma",
-        buttonText: "“Yeah!Maybe, just maybe, it's a concept that's similar to a zero in mathematics. In analog, that's God. In digital, it's zero.”",
-        paragraph: "../src/icons/tabs/2.jpg"
+        buttonText: "reboot the universe",
+        paragraph: "In analog, that's God. In digital, it's zero."
     }
 ];
 
 for (var i = 0, l = Object.keys(dict).length; i < l; i++) {
     $(contPath+"_"+i).append(
-        '<paragraph class="tabs__img tabs__img_content" src="' + dict[i].paragraph + '" >' +
-        '<blockquote class="tabs__text tabs__text_sm tabs__text_lg">' + dict[i].buttonText + '</blockquote>' +
-        '<h3 class="tabs__text tabs__text_name  tabs__text_sm tabs__text_lg">' + dict[i].title + '</h3>' );
+        '<div class="row main_sticked_title"><div class="col d-flex justify-content-center"><h1 class="main__title">' + dict[i].title + '</h1></div></div>' +
+        '<div class="row main_sticked_button    "><div class="col"><button class="button button_primary ">' + dict[i].buttonText + '</button></div></div>' +
+        '<div class="row  main_sticked_paragraph"><div class="col d-flex justify-content-center"><p class="main__paragraph">' + dict[i].paragraph + '</p></div></div>' );
 }
-tabFunction('1');
+tabFunction('0');
 function tabFunction(id) {
     id = parseInt(id);
     $(contPath).hide();
     $(contPath+"_"+id).show();
+    $(".main__input").css("background", "none");
+    $("#"+id).css("background-color", "white");
 }
 
 main__bar.click((e) => {
